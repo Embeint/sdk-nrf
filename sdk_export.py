6f4888392597b9b21d93db3c27d3a2f3cfa11d9c
Jordan Yates <jordan@embeint.com>
2024-04-06 12:49:27 +1000

    treewide: initial export @ v2.6.0
    Initial export of `sdk-nrf` upstream.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ sdk_export.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import argparse
+import shutil
+import pathlib
+
+def tree_replace(upstream: pathlib.Path, output: pathlib.Path, path):
+    shutil.rmtree(output.joinpath(*path), ignore_errors=True)
+    shutil.copytree(upstream.joinpath(*path), output.joinpath(*path))
+
+def export(upstream: pathlib.Path, output: pathlib.Path):
+    tree_replace(upstream, output, ['drivers', 'entropy'])
+    tree_replace(upstream, output, ['modules', 'nrfxlib'])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('sdk-nrf exporter')
+    parser.add_argument('--upstream', type=str, required=True, help='Upstream repository')
+    args = parser.parse_args()
+
+    export(pathlib.Path(args.upstream), pathlib.Path(__file__).parent)
