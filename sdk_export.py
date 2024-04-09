ef544914d4993a25c1c9b7a8b3591e297a20a8f1
Jordan Yates <jordan@embeint.com>
2024-04-10 09:25:13 +1000

    drivers: wifi: import
    Import nRF700x WiFi drivers and other dependencies:
     * hostap
     * mpsl (for radio co-existance)
     * bluetooth (for mpsl Kconfig)
    
    The nRF700x driver is modified to link to the upstream WiFi connectivity
    manager, not the nRF specific one.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- sdk_export.py
+++ sdk_export.py
@@ -8,10 +8,23 @@ def tree_replace(upstream: pathlib.Path, output: pathlib.Path, path):
     shutil.rmtree(output.joinpath(*path), ignore_errors=True)
     shutil.copytree(upstream.joinpath(*path), output.joinpath(*path))
 
+def file_replace(upstream: pathlib.Path, output: pathlib.Path, path):
+    output_file = output.joinpath(*path)
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(upstream.joinpath(*path), output_file)
+
 def export(upstream: pathlib.Path, output: pathlib.Path):
     tree_replace(upstream, output, ['drivers', 'entropy'])
+    tree_replace(upstream, output, ['drivers', 'wifi'])
+    tree_replace(upstream, output, ['dts', 'bindings', 'wifi'])
     tree_replace(upstream, output, ['ext', 'oberon'])
     tree_replace(upstream, output, ['modules', 'nrfxlib'])
+    tree_replace(upstream, output, ['modules', 'hostap'])
+    tree_replace(upstream, output, ['subsys', 'bluetooth'])
+    tree_replace(upstream, output, ['subsys', 'mpsl'])
+    file_replace(upstream, output, ['cmake', 'version.cmake'])
+    file_replace(upstream, output, ['ncs_version.h.in'])
+    file_replace(upstream, output, ['VERSION'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('sdk-nrf exporter')
